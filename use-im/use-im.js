4a60895aeef43245c29117d3951e9c774f1ae80a
zhaojintian <729234283@qq.com>
2022-07-06 15:13:24 +0800

    update

---
+++ use-im/use-im.js
@@ -0,0 +1,47 @@
+import { useWs } from "@/composables/use-ws";
+
+export const useIm = () => {
+  const ws = useWs({ url: "ws://localhost:3002/" });
+
+  const events = { ...ws.events };
+
+  let heartbeatTimer = null;
+
+  const initialize = () => {
+    ws.initialize();
+
+    heartbeatTimer = setInterval(() => {
+      ws.heartbeat.reset();
+      ws.heartbeat.start();
+    }, 5 * 1000);
+
+    ws.emitter.on(events.pingOk, () => {
+      ws.heartbeat.reset();
+    });
+
+    ws.emitter.on(events.error, () => {
+      ws.heartbeat.reset();
+      ws.heartbeat.start();
+    });
+
+    ws.emitter.on(events.close, () => {
+      ws.heartbeat.reset();
+      ws.heartbeat.start();
+    });
+  };
+
+  const destroy = () => {
+    clearInterval(heartbeatTimer);
+    ws.emitter.all.clear();
+  };
+
+  return {
+    events,
+    ws,
+    emitter: ws.emitter,
+    ready: ws.ready,
+    send: ws.send,
+    initialize,
+    destroy,
+  };
+};
---
+++ use-im/use-ws.js
@@ -0,0 +1,95 @@
+import mitt from "mitt";
+import webSocket from "plus-websocket";
+
+export const useWs = ({ url, headers }) => {
+  let instance = null;
+
+  const events = {
+    open: "open",
+    error: "error",
+    close: "close",
+    ping: "ping",
+    pingOk: "pingOk",
+  };
+
+  const emitter = mitt();
+
+  let connected = false;
+
+  let isReady = false;
+
+  let pingTimer = null;
+
+  const initialize = () => {
+    instance = webSocket.connectSocket({ url, complete: () => {} });
+
+    instance.onOpen(() => {
+      connected = true;
+      emitter.emit("open");
+    });
+
+    instance.onError((error) => {
+      connected = false;
+      emitter.emit("error", error);
+    });
+
+    instance.onClose((res) => {
+      connected = false;
+      emitter.emit("close", res);
+    });
+
+    instance.onMessage((res) => {
+      const { event, data } = JSON.parse(res.data);
+      emitter.emit(event, data);
+    });
+  };
+
+  const reconnect = () => {
+    instance.close();
+    initialize();
+  };
+
+  const ready = (cb) => {
+    if (connected) {
+      cb();
+    } else {
+      emitter.on("open", () => {
+        if (!isReady) {
+          cb();
+          isReady = true;
+        }
+      });
+    }
+  };
+
+  const send = ({ event, data }) => {
+    data.headers = headers;
+
+    instance.send({ data: JSON.stringify({ event, data }) });
+  };
+
+  const ping = () => {
+    send({ event: events.ping, data: {} });
+  };
+
+  const heartbeat = {
+    reset() {
+      clearTimeout(pingTimer);
+    },
+    start() {
+      connected && ping();
+      pingTimer = setTimeout(reconnect, 3 * 1000);
+    },
+  };
+
+  return {
+    instance,
+    emitter,
+    events,
+    initialize,
+    reconnect,
+    ready,
+    send,
+    heartbeat,
+  };
+};
